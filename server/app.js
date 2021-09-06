a85025882dfde91210231bf236bf180a2dd8962e
Hoda Shobaki <shobkihoda@gmail.com>
2021-09-07 02:27:26 +0300

    routes

--- server/app.js
+++ server/app.js
@@ -8,7 +8,10 @@ app.use(express.json());
 mongoose.connect("mongodb://localhost:27017/mernauth",{useNewUrlParser:true,
 useUnifiedTopology:true},()=>{
     console.log("successfuly connected");
-})
+});
+ 
+const userRouter=require("./routes/User");
+app.use("/user",userRouter);
 
 
 app.listen(8000,()=>{
