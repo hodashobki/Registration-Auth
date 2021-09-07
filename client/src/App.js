66eb2850faeeebcc785447ec17a9376ac12624b4
Hoda Shobaki <shobkihoda@gmail.com>
2021-09-08 01:15:40 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,25 @@ import Home from './Components/Home'
 import Login from "./Components/Login";
 import Register from "./Components/Register";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Todos from "./Components/Todos";
+import Admin from "./Components/Admin";
+import PrivateRoute from './hocs/PrivateRoute';
+import UnPrivateRoute from './hocs/UnPrivateRoute';
 
 function App() {
   return (
     <Router>
     <Navbar/>
     <Route exact path="/" component={Home}/>
-    <Route path="/Login" component={Login}/>
-    <Route path="/Register" component={Register}/>
+    {/* <Route path="/Login" component={Login}/>
+    <Route path="/Register" component={Register}/> */}
+  
     
     
-    {/* <UnPrivateRoute path="/login" component={Login}/> */}
-    {/* <UnPrivateRoute path="/register" component={Register}/>
+    <UnPrivateRoute path="/login" component={Login}/>
+    <UnPrivateRoute path="/register" component={Register}/>
     <PrivateRoute path="/todos" roles={["user","admin"]} component={Todos}/>
-    <PrivateRoute path="/admin" roles={["admin"]} component={Admin}/> */}
+    <PrivateRoute path="/admin" roles={["admin"]} component={Admin}/>
   </Router>
   );
 }
